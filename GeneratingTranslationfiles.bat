bafc515855e72cb32440a3920d1563e4be3b02f5
conixtract <conixtract@gmail.com>
2020-03-25 19:57:16 +0100

    Ordnerstrultur und Batfile erneuert

--- GeneratingTranslationfiles.bat
+++ GeneratingTranslationfiles.bat
@@ -1,16 +1,18 @@
 @ECHO OFF
+MKDIR %~dp0bin\Debug\data\
+MKDIR %~dp0bin\Release\data\
 ECHO generating translation files at %~dp0bin
 ECHO generating german
-COPY %~dp0Sprache\deutsch.ini %~dp0bin\Debug\data\
-COPY %~dp0Sprache\deutsch.ini %~dp0bin\Release\data\
+COPY %~dp0data\languages\deutsch.ini %~dp0bin\Debug\data\
+COPY %~dp0data\languages\deutsch.ini %~dp0bin\Release\data\
 ECHO generating english
-COPY %~dp0Sprache\englisch.ini %~dp0bin\Debug\data\
-COPY %~dp0Sprache\englisch.ini %~dp0bin\Release\data\
+COPY %~dp0data\languages\englisch.ini %~dp0bin\Debug\data\
+COPY %~dp0data\languages\englisch.ini %~dp0bin\Release\data\
 ECHO generating images
-COPY %~dp0Images\basketball.png %~dp0bin\Release\data\
-COPY %~dp0Images\basketball.png %~dp0bin\Debug\data\
-COPY %~dp0Images\soccer.png %~dp0bin\Release\data\
-COPY %~dp0Images\soccer.png %~dp0bin\Debug\data\
-COPY %~dp0Images\search.png %~dp0bin\Release\data\
-COPY %~dp0Images\search.png %~dp0bin\Debug\data\
+COPY %~dp0data\images\basketball.png %~dp0bin\Release\data\
+COPY %~dp0data\images\basketball.png %~dp0bin\Debug\data\
+COPY %~dp0data\images\soccer.png %~dp0bin\Release\data\
+COPY %~dp0data\images\soccer.png %~dp0bin\Debug\data\
+COPY %~dp0data\images\search.png %~dp0bin\Release\data\
+COPY %~dp0data\images\search.png %~dp0bin\Debug\data\
 ECHO finished generating
